1c7341df14179b801ee8760c50763318bd53d90a
杨蕴琦 <3118731617@qq.com>
2017-07-28 13:40:28 +0800

    add project

---
+++ yyq-project-parent/yyq-project-workbanch/src/main/resources/sql/sell.sql
@@ -0,0 +1,51 @@
+CREATE TABLE `product_info` (
+  `product_id` VARCHAR(32) NOT NULL,
+  `product_name` VARCHAR(64) NOT NULL,
+  `product_price` DECIMAL(8, 2) NOT NULL,
+  `product_stock` INT NOT NULL,
+  `product_description` VARCHAR(64),
+  `product_icon` VARCHAR(512),
+  `category_type` INT NOT NULL,
+  `create_time` TIMESTAMP  NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`product_id`)
+);
+
+CREATE TABLE `product_category` (
+  `category_id` INT NOT NULL AUTO_INCREMENT,
+  `category_name` VARCHAR(64) NOT NULL,
+  `category_type` INT NOT NULL,
+  `create_time` TIMESTAMP  NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`category_id`),
+  UNIQUE KEY `uqe_category_type` (`category_type`)
+);
+
+CREATE TABLE `order_master` (
+  `order_id` VARCHAR(32) NOT NULL,
+  `buyer_name` VARCHAR(32) NOT NULL,
+  `buyer_phone` VARCHAR(32) NOT NULL,
+  `buyer_address` VARCHAR(128) NOT NULL,
+  `buyer_openid` VARCHAR(64) NOT NULL,
+  `order_amount` DECIMAL(8, 2) NOT NULL,
+  `order_status` TINYINT(3) NOT NULL DEFAULT 0,
+  `pay_status` TINYINT(3) NOT NULL DEFAULT 0,
+  `create_time` TIMESTAMP  NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`order_id`),
+  KEY `idx_buyer_openid` (`buyer_openid`)
+);
+
+CREATE TABLE `order_detail` (
+  `detail_id` VARCHAR(32) NOT NULL ,
+  `order_id` VARCHAR(32) NOT NULL ,
+  `product_id` VARCHAR(32) NOT NULL,
+  `product_name` VARCHAR(64) NOT NULL,
+  `product_price` DECIMAL(8, 2) NOT NULL,
+  `product_quantity` INT NOT NULL,
+  `product_icon` VARCHAR(512),
+  `create_time` TIMESTAMP  NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`detail_id`),
+  KEY `idx_order_id` (`order_id`)
+);
---
+++ yyq-project-product/src/main/java/com/yyq/project/product/ProjectProductApplication.java
@@ -0,0 +1,12 @@
+package com.yyq.project.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectProductApplication.class, args);
+	}
+}
